867df955d1fc6b83d55762853be8f5f2e57a1436
0-x-14 <0x14@daum.net>
2024-09-30 16:18:20 +0900

    [Feat] Conjured 아이템 처리 추가

--- Java/src/main/java/com/gildedrose/Update/UpdateLogicFactory.java
+++ Java/src/main/java/com/gildedrose/Update/UpdateLogicFactory.java
@@ -4,6 +4,11 @@ import com.gildedrose.Item;
 
 public class UpdateLogicFactory {
     public static UpdateLogic getUpdateLogic(Item item) {
+        if (item.name.contains("Conjured")) {
+            // Conjured 아이템인 경우 switch문에서 판별 불가능하므로 별도 처리
+            return new UpdateConjuredItemLogic();
+        }
+
         switch (item.name) {
             case "Aged Brie":
                 return new UpdateAgedBrieLogic();
