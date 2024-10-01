f6d357fd01d8d3bab1dad571864fedae6f0198ca
chyun <och2510@gmail.com>
2024-10-01 12:12:02 +0900

    style: 불필요한 import 삭제

--- Java/src/main/java/com/gildedrose/update/SulfurasUpdate.java
+++ Java/src/main/java/com/gildedrose/update/SulfurasUpdate.java
@@ -2,10 +2,6 @@ package com.gildedrose.update;
 
 import com.gildedrose.Item;
 
-import java.lang.Math;
-
-import static com.gildedrose.constant.ItemPropertyConstant.*;
-
 public class SulfurasUpdate implements Update {
     @Override
     public void update(Item item) {
