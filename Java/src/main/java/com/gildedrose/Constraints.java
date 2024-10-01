501427773b651ff9a95da992dde035238cdf7cb7
윤섭 <54024213+F-UMI@users.noreply.github.com>
2024-10-02 01:13:00 +0900

    :sparkles: Feat: 제약사항 설정

---
+++ Java/src/main/java/com/gildedrose/Constraints.java
@@ -0,0 +1,24 @@
+package com.gildedrose;
+
+public enum Constraints {
+    QUALITY_OF_SULFURAS(80),
+    QUALITY_UPPER_LIMIT(50),
+    QUALITY_LOWER_LIMIT(0),
+    SELLIN_LOWER_LIMIT(0),
+    QUALITY_INCREASE_DEFAULT(1),
+    QUALITY_INCREASE_BACKSTAGE_PASSES_LEFT_10(2),
+    QUALITY_INCREASE_BACKSTAGE_PASSES_LEFT_5(3),
+    QUALITY_DECREASE_DEFAULT(1),
+    QUALITY_DECREASE_CONJURED(2);
+
+    private final int value;
+
+    Constraints(int value) {
+        this.value = value;
+    }
+
+    public int value() {
+        return value;
+    }
+
+}
