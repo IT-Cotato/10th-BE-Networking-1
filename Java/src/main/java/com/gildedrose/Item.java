3f83f672d6a8eec29751f1cee204a8a788377390
jeongchanmin <jjw421412@naver.com>
2024-10-03 21:25:05 +0900

    :sparkles: [Feat] Item 생성자 및 주석 추가

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -4,9 +4,9 @@ public class Item {
 
     public String name;
 
-    public int sellIn;
+    public int sellIn; // 아이템을 판매해야 하는 남은 기한
 
-    public int quality;
+    public int quality; // 아이템의 가치
 
     public Item(String name, int sellIn, int quality) {
         this.name = name;
@@ -14,8 +14,14 @@ public class Item {
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
+    public Item(String name, int sellIn) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = 0;
+    }
+
+    @Override
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 }
