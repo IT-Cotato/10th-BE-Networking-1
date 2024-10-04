e74e290a3c91c74e64ebc11ffa1941e56e5fc2f1
u-genuine <u_genuine@naver.com>
2024-10-04 17:29:23 +0900

    Conjured 아이템 전략 추가

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 import com.gildedrose.strategy.AgedBrieUpdateStrategy;
 import com.gildedrose.strategy.BackstagePassesUpdateStrategy;
+import com.gildedrose.strategy.ConjuredItemUpdateStrategy;
 import com.gildedrose.strategy.StandardItemUpdateStrategy;
 import com.gildedrose.strategy.SulfurasUpdateStrategy;
 
@@ -18,6 +19,7 @@ class GildedRose {
         this.strategies.put("Backstage passes to a TAFKAL80ETC concert", new BackstagePassesUpdateStrategy());
         this.strategies.put("Sulfuras, Hand of Ragnaros", new SulfurasUpdateStrategy());
         this.strategies.put("default", new StandardItemUpdateStrategy());
+        this.strategies.put("Conjured Mana Cake", new ConjuredItemUpdateStrategy());
     }
 
     public void updateQuality() {
