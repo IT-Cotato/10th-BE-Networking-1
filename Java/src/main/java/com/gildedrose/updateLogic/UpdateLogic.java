ca7ae3407353253607ebfcd7d5c9dbf9cc676e26
Yeongchan Kim <hades1@kakao.com>
2024-10-05 20:46:09 +0900

    refactor: UpdateLogic에서 고정 변수 생성 및 증가, 감소 메서드 생성

--- Java/src/main/java/com/gildedrose/updateLogic/UpdateLogic.java
+++ Java/src/main/java/com/gildedrose/updateLogic/UpdateLogic.java
@@ -3,8 +3,24 @@ package com.gildedrose.updateLogic;
 import com.gildedrose.Item;
 
 public abstract class UpdateLogic {
+    public static final int MAX_QUALITY = 50;
+    public static final int MIN_QUALITY = 0;
+    public static final int DOWN_MULTIPLE = 2;
+
     protected void decreaseSellIn(Item item) {
         item.sellIn -= 1;
+    }
+
+    protected void decreaseQuality(Item item, int downAmount) {
+        if (item.sellIn < 0) {
+            item.quality = Math.max(item.quality - downAmount * DOWN_MULTIPLE, MIN_QUALITY);
+        } else {
+            item.quality = Math.max(item.quality - downAmount, MIN_QUALITY);
+        }
+    }
+
+    protected void increaseQuality(Item item) {
+        item.quality = Math.min(item.quality + 1, MAX_QUALITY);
     }
 
     public abstract void update(Item item);
