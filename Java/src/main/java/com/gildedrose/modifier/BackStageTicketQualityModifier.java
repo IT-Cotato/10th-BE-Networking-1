33ef95dd62755530434ed9c569e626990564a513
supsup_hae <zksdbstjq@gmail.com>
2024-10-25 13:46:35 +0900

    :recycle: Refactor: BACKSTAGE_PASSES_LEFT 상수값 선언

--- Java/src/main/java/com/gildedrose/modifier/BackStageTicketQualityModifier.java
+++ Java/src/main/java/com/gildedrose/modifier/BackStageTicketQualityModifier.java
@@ -8,10 +8,10 @@ public class BackStageTicketQualityModifier implements ItemQualityModifier {
     @Override
     public void modify(Item item) {
 
-        if (item.sellIn > 10) {
+        if (item.sellIn > Constraints.BACKSTAGE_PASSES_LEFT_10.value()) {
             item.quality = Math.min(Constraints.QUALITY_UPPER_LIMIT.value(),
                 item.quality + Constraints.QUALITY_DECREASE_DEFAULT.value());
-        } else if (item.sellIn > 5) {
+        } else if (item.sellIn > Constraints.BACKSTAGE_PASSES_LEFT_5.value()) {
             item.quality = Math.min(Constraints.QUALITY_UPPER_LIMIT.value(),
                 item.quality + Constraints.QUALITY_INCREASE_BACKSTAGE_PASSES_LEFT_10.value());
         } else if (item.sellIn >= Constraints.SELLIN_LOWER_LIMIT.value()) {
@@ -24,3 +24,4 @@ public class BackStageTicketQualityModifier implements ItemQualityModifier {
         item.sellIn--;
     }
 }
+
