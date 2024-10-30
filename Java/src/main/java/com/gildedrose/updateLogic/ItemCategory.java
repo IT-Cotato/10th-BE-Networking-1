bd94f0819ef531fe6dad5145be8618c12a8d942e
Yeongchan Kim <hades1@kakao.com>
2024-10-30 21:41:57 +0900

    refactor: ItemCategory의 value 연결

--- Java/src/main/java/com/gildedrose/updateLogic/ItemCategory.java
+++ Java/src/main/java/com/gildedrose/updateLogic/ItemCategory.java
@@ -1,5 +1,18 @@
 package com.gildedrose.updateLogic;
 
 public enum ItemCategory {
-    BRIE, PASS, CONJURED, SULFURAS, ELSE
+    BRIE("Aged Brie"),
+    PASS("Backstage passes to a "),
+    CONJURED("Conjured "),
+    SULFURAS("Sulfuras");
+
+    private final String value;
+
+    ItemCategory(String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
 }
