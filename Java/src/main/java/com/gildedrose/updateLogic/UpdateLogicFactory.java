e4c2671a0af6bb8530f8dcdf910420a8c3538c50
Yeongchan Kim <hades1@kakao.com>
2024-10-30 22:05:59 +0900

    refactor: UpdateLogicFactory에서 itemMap 제거 및 수정

--- Java/src/main/java/com/gildedrose/updateLogic/UpdateLogicFactory.java
+++ Java/src/main/java/com/gildedrose/updateLogic/UpdateLogicFactory.java
@@ -2,31 +2,28 @@ package com.gildedrose.updateLogic;
 
 import static com.gildedrose.updateLogic.ItemCategory.*;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class UpdateLogicFactory {
     private final Map<ItemCategory, UpdateLogic> updateLogicMap;
-    private final Map<String, ItemCategory> itemMap;
 
     public UpdateLogicFactory() {
         updateLogicMap = new HashMap<>();
-        itemMap = Map.ofEntries(Map.entry("Aged Brie", BRIE), Map.entry("Backstage passes", PASS),
-            Map.entry("Conjured", CONJURED), Map.entry("Sulfuras", SULFURAS));
 
         updateLogicMap.put(BRIE, new BrieUpdateLogic());
         updateLogicMap.put(PASS, new PassesUpdateLogic());
         updateLogicMap.put(CONJURED, new ConjuredUpdateLogic());
         updateLogicMap.put(SULFURAS, new SulfurasUpdateLogic());
-        updateLogicMap.put(ELSE, new ElseUpdateLogic());
+        updateLogicMap.put(NORMAL, new NormalUpdateLogic());
     }
 
     public UpdateLogic findUpdateLogic(String name) {
-        for (String key : itemMap.keySet()) {
-            if (name.contains(key)) {
-                return updateLogicMap.get(itemMap.get(key));
-            }
-        }
-        return updateLogicMap.get(ELSE);
+        ItemCategory itemCategory = Arrays.stream(ItemCategory.values())
+            .filter(category -> name.contains(category.value()))
+            .findFirst()
+            .orElse(NORMAL);
+        return updateLogicMap.get(itemCategory);
     }
 }
