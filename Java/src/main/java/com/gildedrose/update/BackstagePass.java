7d888ef406f8bec68cef89bc353c823b37bc0bb2
Ye jin Shin <108514552+shinyj0@users.noreply.github.com>
2024-11-01 03:41:04 +0900

    refactor: 매직넘버 관리

--- Java/src/main/java/com/gildedrose/update/BackstagePass.java
+++ Java/src/main/java/com/gildedrose/update/BackstagePass.java
@@ -1,26 +1,31 @@
 package com.gildedrose.update;
 
+import static com.gildedrose.update.ItemConstants.*;
+
 import com.gildedrose.Item;
 
 public class BackstagePass implements UpdateLogic{
 
+    private static final int BACKSTAGE_SELL_IN_THRESHOLD_10 = 10;
+    private static final int BACKSTAGE_SELL_IN_THRESHOLD_5 = 5;
+
     @Override
     public void update(Item item) {
 
-        if (item.quality < 50) {
+        if (item.quality < QUALITY_MAX) {
             item.quality++;
-            if (item.sellIn < 11 && item.quality < 50) {
+            if (item.sellIn <= BACKSTAGE_SELL_IN_THRESHOLD_10 && item.quality < QUALITY_MAX) {
                 item.quality++;
             }
-            if (item.sellIn < 6 && item.quality < 50) {
+            if (item.sellIn <= BACKSTAGE_SELL_IN_THRESHOLD_5 && item.quality < QUALITY_MAX) {
                 item.quality++;
             }
         }
 
         item.sellIn--;
 
-        if (item.sellIn < 0) {
-            item.quality = 0;
+        if (item.sellIn < QUALITY_MIN) {
+            item.quality = QUALITY_MIN;
         }
     }
 }
--- Java/src/main/java/com/gildedrose/update/DefaultItem.java
+++ Java/src/main/java/com/gildedrose/update/DefaultItem.java
@@ -1,19 +1,21 @@
 package com.gildedrose.update;
 
+import static com.gildedrose.update.ItemConstants.*;
+
 import com.gildedrose.Item;
 
 public class DefaultItem implements UpdateLogic{
 
     @Override
     public void update(Item item) {
-        if (item.quality > 0) {
-            item.quality--;
+        if (item.quality > QUALITY_MIN) {
+            item.quality -= DEFAULT_QUALITY_DECREMENT;
         }
 
         item.sellIn--;
 
-        if (item.sellIn < 0 && item.quality > 0) {
-            item.quality--;
+        if (item.sellIn < 0 && item.quality > QUALITY_MIN) {
+            item.quality -= DEFAULT_QUALITY_DECREMENT;
         }
     }
 }
